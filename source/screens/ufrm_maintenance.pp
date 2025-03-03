c656168a1e9abb7e6ca00219eab1075085e9b361
Christian Beier <109050697+cbeier-studio@users.noreply.github.com>
2025-03-03 17:00:27 -0300

    Improved maintenance dialog and first tools #75

--- source/screens/ufrm_maintenance.pp
+++ source/screens/ufrm_maintenance.pp
@@ -21,7 +21,7 @@ unit ufrm_maintenance;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls, Buttons, ComCtrls,
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls, Buttons, ComCtrls, DateUtils,
   attabs, atshapelinebgra, BCPanel;
 
 type
@@ -29,56 +29,73 @@ type
   { TfrmMaintenance }
 
   TfrmMaintenance = class(TForm)
-    icoRewriteGeoHierarchy: TImage;
-    icoRewriteGeoHierarchy1: TImage;
-    iIcons: TImageList;
-    iIconsDark: TImageList;
-    lblRewriteGeoHierarchy: TLabel;
-    lblRewriteGeoHierarchy1: TLabel;
-    lblTitleGeoHierarchy: TLabel;
-    lblTitleGeoHierarchy1: TLabel;
-    pGeo1: TPanel;
-    pRewriteGeoHierarchy1: TBCPanel;
-    pRewriteTaxaHierarchy: TBCPanel;
-    pRewriteGeoHierarchy: TBCPanel;
-    pRewriteTaxaSequence: TBCPanel;
-    cbTaxaHierarchy: TComboBox;
-    cbTaxaSequence: TComboBox;
-    icoRewriteTaxaHierarchy: TImage;
-    icoRewriteTaxaSequence: TImage;
-    lblRewriteTaxaHierarchy: TLabel;
-    lblRewriteTaxaSequence: TLabel;
-    pGeo: TPanel;
-    sbRewriteIndividualsFullname: TBitBtn;
-    sbRewriteTaxaHierarchy: TBitBtn;
-    sbRewriteGeoHierarchy: TBitBtn;
-    sbRewriteTaxaSequence: TBitBtn;
-    scrollGeo: TScrollBox;
-    tabGeo: TPage;
-    pRecreateImageThumbnails: TBCPanel;
     icoRecreateImageThumbnails: TImage;
+    icoDatabaseBackup: TImage;
+    icoCheckDatabaseIntegrity: TImage;
+    icoCheckOptimizeDatabase: TImage;
+    icoDatabaseIntegrity: TImage;
+    icoOptimizeDatabase: TImage;
+    icoSettingsBackup: TImage;
+    icoFactoryReset: TImage;
+    icoManageLogs: TImage;
+    icoClearTemporaryFiles: TImage;
+    icoDiagnostic: TImage;
+    icoCheckDatabaseBackup: TImage;
+    iIcons: TImageList;
+    iCheck: TImageList;
+    iIconsDark: TImageList;
+    iCheckDark: TImageList;
     lblRecreateImageThumbnails: TLabel;
-    lblTitleTaxaHierarchy: TLabel;
-    lblTitleImageThumbnails: TLabel;
+    lblDatabaseBackup: TLabel;
+    lblDatabaseIntegrity: TLabel;
+    lblOptimizeDatabase: TLabel;
+    lblSettingsBackup: TLabel;
+    lblFactoryReset: TLabel;
+    lblManageLogs: TLabel;
+    lblClearTemporaryFiles: TLabel;
+    lblDiagnostic: TLabel;
+    OpenDlg: TOpenDialog;
+    pContent: TPanel;
     lineBottom: TShapeLineBGRA;
     pBottom: TPanel;
-    pImages: TPanel;
+    pRecreateImageThumbnails: TBCPanel;
+    pDatabaseBackup: TBCPanel;
+    pDatabaseIntegrity: TBCPanel;
+    pOptimizeDatabase: TBCPanel;
+    pSettingsBackup: TBCPanel;
+    pFactoryReset: TBCPanel;
+    pManageLogs: TBCPanel;
+    pClearTemporaryFiles: TBCPanel;
+    pDiagnostic: TBCPanel;
     sbClose: TButton;
     sbRecreateImageThumbnails: TBitBtn;
-    scrollImages: TScrollBox;
-    tabImages: TPage;
-    pTaxonomy: TPanel;
-    PG: TNotebook;
-    scrollTaxonomy: TScrollBox;
-    tabIndividuals: TPage;
-    tabTaxonomy: TPage;
-    tvMenu: TTreeView;
+    sbRestoreDatabase: TBitBtn;
+    sbBackupDatabase: TBitBtn;
+    sbCheckDatabaseIntegrity: TBitBtn;
+    sbOptimizeDatabase: TBitBtn;
+    sbRestoreSettings: TBitBtn;
+    sbFactoryReset: TBitBtn;
+    sbClearLogs: TBitBtn;
+    sbClearTemporaryFiles: TBitBtn;
+    sbDiagnostic: TBitBtn;
+    sbBackupSettings: TBitBtn;
+    titleRecreateThumbnails: TLabel;
+    titleDatabaseBackup: TLabel;
+    titleDatabaseIntegrity: TLabel;
+    titleOptimizeDatabase: TLabel;
+    titleSettingsBackup: TLabel;
+    titleFactoryReset: TLabel;
+    titleManageLogs: TLabel;
+    titleClearTemporaryFiles: TLabel;
+    titleDiagnostic: TLabel;
     procedure FormShow(Sender: TObject);
+    procedure sbBackupDatabaseClick(Sender: TObject);
+    procedure sbClearLogsClick(Sender: TObject);
     procedure sbRecreateImageThumbnailsClick(Sender: TObject);
-    procedure sbRewriteTaxaHierarchyClick(Sender: TObject);
-    procedure tvMenuSelectionChanged(Sender: TObject);
+    procedure sbRestoreDatabaseClick(Sender: TObject);
   private
     procedure ApplyDarkMode;
+    procedure CheckDatabaseBackup;
   public
 
   end;
@@ -88,44 +105,98 @@ var
 
 implementation
 
-uses cbs_locale, cbs_dialogs, cbs_taxonomy, cbs_blobs, cbs_graphics, cbs_fullnames, cbs_themes, uDarkStyleParams;
+uses
+  cbs_locale, cbs_global, cbs_dialogs, cbs_backup, cbs_blobs, cbs_graphics, cbs_data,
+  cbs_themes, uDarkStyleParams;
 
 {$R *.lfm}
 
 { TfrmMaintenance }
 
-procedure TfrmMaintenance.sbRewriteTaxaHierarchyClick(Sender: TObject);
-begin
-  case cbTaxaHierarchy.ItemIndex of
-    0: RewriteTaxonHierarchy;
-    1: ;
-  end;
-end;
-
-procedure TfrmMaintenance.tvMenuSelectionChanged(Sender: TObject);
-begin
-  PG.PageIndex := tvMenu.Selected.Index;
-end;
-
 procedure TfrmMaintenance.ApplyDarkMode;
 begin
-  tvMenu.Images := iIconsDark;
-  icoRewriteTaxaHierarchy.Images := iIconsDark;
-  icoRewriteTaxaSequence.Images := iIconsDark;
-  icoRewriteGeoHierarchy.Images := iIconsDark;
-  icoRewriteGeoHierarchy1.Images := iIconsDark;
+  icoDatabaseBackup.Images := iIconsDark;
+  icoDatabaseIntegrity.Images := iIconsDark;
+  icoOptimizeDatabase.Images := iIconsDark;
+  icoSettingsBackup.Images := iIconsDark;
+  icoFactoryReset.Images := iIconsDark;
+  icoManageLogs.Images := iIconsDark;
+  icoClearTemporaryFiles.Images := iIconsDark;
+  icoDiagnostic.Images := iIconsDark;
   icoRecreateImageThumbnails.Images := iIconsDark;
 
-  pRecreateImageThumbnails.Background.Color := clCardBGDefaultDark;
+  icoCheckDatabaseBackup.Images := iCheckDark;
+  icoCheckDatabaseIntegrity.Images := iCheckDark;
+  icoCheckOptimizeDatabase.Images := iCheckDark;
+
+  pDatabaseBackup.Background.Color := clSolidBGBaseDark;
+  pDatabaseBackup.Border.Color := clSystemSolidNeutralFGDark;
+  pDatabaseIntegrity.Background.Color := clSolidBGBaseDark;
+  pDatabaseIntegrity.Border.Color := clSystemSolidNeutralFGDark;
+  pOptimizeDatabase.Background.Color := clSolidBGBaseDark;
+  pOptimizeDatabase.Border.Color := clSystemSolidNeutralFGDark;
+  pSettingsBackup.Background.Color := clSolidBGBaseDark;
+  pSettingsBackup.Border.Color := clSystemSolidNeutralFGDark;
+  pFactoryReset.Background.Color := clSolidBGBaseDark;
+  pFactoryReset.Border.Color := clSystemSolidNeutralFGDark;
+  pManageLogs.Background.Color := clSolidBGBaseDark;
+  pManageLogs.Border.Color := clSystemSolidNeutralFGDark;
+  pClearTemporaryFiles.Background.Color := clSolidBGBaseDark;
+  pClearTemporaryFiles.Border.Color := clSystemSolidNeutralFGDark;
+  pDiagnostic.Background.Color := clSolidBGBaseDark;
+  pDiagnostic.Border.Color := clSystemSolidNeutralFGDark;
+  pRecreateImageThumbnails.Background.Color := clSolidBGBaseDark;
   pRecreateImageThumbnails.Border.Color := clSystemSolidNeutralFGDark;
-  pRewriteGeoHierarchy1.Background.Color := clCardBGDefaultDark;
-  pRewriteGeoHierarchy1.Border.Color := clSystemSolidNeutralFGDark;
-  pRewriteGeoHierarchy.Background.Color := clCardBGDefaultDark;
-  pRewriteGeoHierarchy.Border.Color := clSystemSolidNeutralFGDark;
-  pRewriteTaxaHierarchy.Background.Color := clCardBGDefaultDark;
-  pRewriteTaxaHierarchy.Border.Color := clSystemSolidNeutralFGDark;
-  pRewriteTaxaSequence.Background.Color := clCardBGDefaultDark;
-  pRewriteTaxaSequence.Border.Color := clSystemSolidNeutralFGDark;
+
+  lblDatabaseBackup.Font.Color := $009F9F9F;
+  lblDatabaseIntegrity.Font.Color := $009F9F9F;
+  lblOptimizeDatabase.Font.Color := $009F9F9F;
+  lblSettingsBackup.Font.Color := $009F9F9F;
+  lblFactoryReset.Font.Color := $009F9F9F;
+  lblManageLogs.Font.Color := $009F9F9F;
+  lblClearTemporaryFiles.Font.Color := $009F9F9F;
+  lblDiagnostic.Font.Color := $009F9F9F;
+  lblRecreateImageThumbnails.Font.Color := $009F9F9F;
+end;
+
+procedure TfrmMaintenance.CheckDatabaseBackup;
+begin
+  icoCheckDatabaseBackup.ImageIndex := 0;
+  icoCheckDatabaseBackup.Hint := 'Automatic backup: ENABLED and DONE';
+
+  ConexaoDB.LoadParams;
+
+  case XSettings.AutomaticBackup of
+    0:
+    begin
+      icoCheckDatabaseBackup.ImageIndex := 1;
+      icoCheckDatabaseBackup.Hint := 'Automatic backup: DISABLED';
+    end;
+    1:
+    begin
+      if DaysBetween(Now, ConexaoDB.LastBackup) >= 1 then
+      begin
+        icoCheckDatabaseBackup.ImageIndex := 1;
+        icoCheckDatabaseBackup.Hint := 'Automatic backup: ENABLED and NOT DONE';
+      end;
+    end;
+    2:
+    begin
+      if DaysBetween(Now, ConexaoDB.LastBackup) >= 7 then
+      begin
+        icoCheckDatabaseBackup.ImageIndex := 1;
+        icoCheckDatabaseBackup.Hint := 'Automatic backup: ENABLED and NOT DONE';
+      end;
+    end;
+    3:
+    begin
+      if DaysBetween(Now, ConexaoDB.LastBackup) >= 30 then
+      begin
+        icoCheckDatabaseBackup.ImageIndex := 1;
+        icoCheckDatabaseBackup.Hint := 'Automatic backup: ENABLED and NOT DONE';
+      end;
+    end;
+  end;
 end;
 
 procedure TfrmMaintenance.FormShow(Sender: TObject);
@@ -133,13 +204,39 @@ begin
   if IsDarkModeEnabled then
     ApplyDarkMode;
 
-  tvMenu.Selected := tvMenu.Items.GetFirstNode;
+  CheckDatabaseBackup;
+end;
+
+procedure TfrmMaintenance.sbBackupDatabaseClick(Sender: TObject);
+begin
+  VacuumIntoBackup; //NewBackup;
+
+  CheckDatabaseBackup;
+end;
+
+procedure TfrmMaintenance.sbClearLogsClick(Sender: TObject);
+var
+  FLog: String;
+begin
+  FLog := ConcatPaths([AppDataDir, LogFile]);
+  if FileExists(FLog) then
+    DeleteFile(FLog);
 end;
 
 procedure TfrmMaintenance.sbRecreateImageThumbnailsClick(Sender: TObject);
 begin
   if MsgDlg(rsTitleRecreateThumbnails, rsRecreateThumbnailsPrompt, mtConfirmation) then
     RecreateThumbnails;
+end;
+
+procedure TfrmMaintenance.sbRestoreDatabaseClick(Sender: TObject);
+begin
+  if not MsgDlg(rsTitleRestore, rsRestoreBackupPrompt, mtConfirmation) then
+    Exit;
+
+  OpenDlg.InitialDir:= XSettings.BackupFolder;
+  if OpenDlg.Execute then
+    RestoreBackup(OpenDlg.FileName);
 end;
 
 end.
